5f19922effb9f5cfc19114e89bc6564de4ec1a56
Mohit singh <123159324+MohitSingh0707@users.noreply.github.com>
2024-11-20 14:31:20 +0530

    changes in login and register file

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,52 +1,65 @@
-import React,{useState} from 'react'
-import {Form,Input,message} from 'antd'
-import { Link,useNavigate } from 'react-router-dom';
-import axios from "axios"
+import React, { useState } from 'react';
+import { Form, Input, message } from 'antd';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import Spinner from '../components/Layout/Spinner';
 
 const Login = () => {
-    const[loading,setLoading]=useState(false);
-    const navigate = useNavigate()
-    // form submit
-    const submitHandler=async(values)=>{
-        try {
-            setLoading(true)
-            const{data}=await axios.post("/users/login",values)
-            setLoading(false)
-            message.success('login success')
-            localStorage.setItem('user',JSON.stringify({...data.user,password:''}))
-            navigate('/')
-        } catch (error) {
-            setLoading(false);
-            message.error("Something went wrong") 
-        }
-    };
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  // Form submit handler
+  const submitHandler = async (values) => {
+    try {
+      setLoading(true);
+      const { data } = await axios.post('http://localhost:8080/api/v1/users/login', values);
+      setLoading(false);
+      message.success('Login successful');
+      localStorage.setItem('user', JSON.stringify({ ...data.user, password: '' }));
+      navigate('/');
+    } catch (error) {
+      setLoading(false);
+      message.error(error.response?.data?.message || 'Something went wrong');
+    }
+  };
+
   return (
-    <>
-      <div className='register-page'>
-        {loading && <Spinner/>}
-        <Form layout='vertical'  onFinish={submitHandler}>
-            <h1>Login Form</h1>
+    <div className="register-page">
+      {loading && <Spinner />}
+      <Form layout="vertical" onFinish={submitHandler}>
+        <h1>Login Form</h1>
 
-            {/* Email */}
-            <Form.Item label='Email' name='email'>
-                <Input type='email'/>
-            </Form.Item>
+        {/* Email */}
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            { required: true, message: 'Please enter your email!' },
+            { type: 'email', message: 'Please enter a valid email!' },
+          ]}
+        >
+          <Input type="email" />
+        </Form.Item>
 
-            {/* Password */}
-            <Form.Item label='Password' name='password'>
-                <Input type='password'/>
-            </Form.Item>
+        {/* Password */}
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please enter your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-            {/* Button */}
-            <div className='d-flex justify-content-between'>
-                <Link to="/register">Not a user ? Click here to register</Link>
-                <button className='btn btn-primary'>Login</button>
-            </div>
-        </Form>
-      </div>
-    </>
-  )
-}
+        {/* Button */}
+        <div className="d-flex justify-content-between">
+          <Link to="/register">Not a user? Click here to register</Link>
+          <button className="btn btn-primary" type="submit">
+            Login
+          </button>
+        </div>
+      </Form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,66 +1,77 @@
 import React, { useState } from 'react';
 import { Form, Input, message } from 'antd';
 import { Link, useNavigate } from 'react-router-dom';
-import axios from "axios";
+import axios from 'axios';
 import Spinner from '../components/Layout/Spinner';
 
 const Register = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
 
-  // form submit
+  // Form submit handler
   const submitHandler = async (values) => {
     try {
       setLoading(true);
       console.log(values);
-      
-      await axios.post('/users/register', values);
-      message.success("Registration Successful");
+
+      await axios.post('http://localhost:8080/api/v1/users/register', values);
+      message.success('Registration successful');
       setLoading(false);
       navigate('/login');
     } catch (error) {
       setLoading(false);
-      message.error("Something went wrong");
+      message.error(error.response?.data?.message || 'Something went wrong');
     }
   };
 
   return (
-    <div className='register-page'>
+    <div className="register-page">
       {loading && <Spinner />}
-      <Form layout='vertical' onFinish={submitHandler}>
+      <Form layout="vertical" onFinish={submitHandler}>
         <h1>Register Form</h1>
 
         {/* Name */}
         <Form.Item
-          label='Name'
-          name='name'
-          rules={[{ required: true, message: 'Please enter your name!' }]}
+          label="Name"
+          name="name"
+          rules={[
+            { required: true, message: 'Please enter your name!' },
+            { min: 3, message: 'Name must be at least 3 characters!' },
+          ]}
         >
-          <Input type='text' />
+          <Input type="text" />
         </Form.Item>
 
         {/* Email */}
         <Form.Item
-          label='Email'
-          name='email'
-          rules={[{ required: true, message: 'Please enter your email!' }, { type: 'email', message: 'Please enter a valid email!' }]}
+          label="Email"
+          name="email"
+          rules={[
+            { required: true, message: 'Please enter your email!' },
+            { type: 'email', message: 'Please enter a valid email!' },
+          ]}
         >
-          <Input type='email' />
+          <Input type="email" />
         </Form.Item>
 
         {/* Password */}
         <Form.Item
-          label='Password'
-          name='password'
-          rules={[{ required: true, message: 'Please enter your password!' }]}
+          label="Password"
+          name="password"
+          rules={[
+            { required: true, message: 'Please enter your password!' },
+            { min: 6, message: 'Password must be at least 6 characters!' },
+          ]}
         >
           <Input.Password />
         </Form.Item>
 
         {/* Button */}
-        <div className='d-flex justify-content-between'>
+        <div className="d-flex justify-content-between">
           <Link to="/login">Already registered? Click here to login</Link>
-          <button className='btn btn-primary' type='submit'>Register</button>
+          <button className="btn btn-primary" type="submit">
+            Register
+          </button>
         </div>
       </Form>
     </div>
@@ -68,3 +79,4 @@ const Register = () => {
 };
 
 export default Register;
+ 
